362411b41571b0bc6eec516a23b640d5377df3af
RomainBoudet <romainoclock@gmail.com>
2022-05-08 17:00:12 +0200

    secure Acces controll Allow Origin server

--- SERVER/server.js
+++ SERVER/server.js
@@ -37,7 +37,7 @@ const db = {
  */
 app.use(bodyParser.json());
 app.use((_, response, next) => {
-  response.header('Access-Control-Allow-Origin', '*');
+  response.header('Access-Control-Allow-Origin', 'http://localhost:8090');
   // response.header('Access-Control-Allow-Credentials', true);
   response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   response.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
